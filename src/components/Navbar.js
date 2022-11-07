58b886182d5b62112598eecffdcdcc40912e61b9
UnbertenKma <mvthang01@gmail.com>
2022-11-08 00:01:07 +0700

    upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div
+      className="
+      h-24 bg-slate-900 text-white text-center leading-[6rem] 
+      text-lg 
+      md:text-3xl
+      lg:text-4xl
+    "
+    >
+      <i className="fa fa-cloud md:my-7 lg:my-7"></i>
+      <span className="font-extrabold">Yeunhac</span>
+    </div>
+  );
+}
